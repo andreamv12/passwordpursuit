720e3cc1935c480cef4280668d3a4cb814e6b28d
andreamv12 <132504131+andreamv12@users.noreply.github.com>
2024-01-31 12:18:48 -0700

    Add files via upload

---
+++ story_content/6eR7Pf86ELG_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.79.30921.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:01.024%0D%0APassword%20Pursuit%0D%0A%0D%0A00:00:01.121%20--%3E%2000:00:02.691%0D%0A%20A%20Virtual%20Escape%20Room%0D%0A%0D%0A"}')
